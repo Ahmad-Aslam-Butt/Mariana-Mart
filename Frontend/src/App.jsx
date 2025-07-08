18275e8c0f961223ab93f447821093cafd1eaef4
Muhammad Ahsan <mahsan0516@gmail.com>
2025-07-09 00:03:51 +0500

    setup routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,33 @@
 import './App.css'
+import {Routes, Route, BrowserRouter, Navigate} from 'react-router-dom'
+import { Dashboard } from './Dashboard'
+import { Admin_Home } from './Admin/Pages/Admin_Home'
+import { Customer_Home } from './Customer/Pages/Customer_Home'
+import { Drop_Ship_Home } from './Dropshipper/Pages/Drop_Ship_Home'
+import { AdminDashboard } from './Admin/Pages/AdminDashboard'
+import { CustomerDashboard } from './Customer/Pages/CustomerDashboard'
+import { DropShipDashboard } from './Dropshipper/Pages/DropShipDashboard'
 
 function App() {
 
   return (
-    <div>
-      <h1 className='flex justify-center'>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/admin' element={<Admin_Home />}>
+          <Route index element={<Navigate to='dashboard' replace />} />
+          <Route path='dashboard' element={<AdminDashboard />} />
+        </Route>
+        <Route path='/customer' element={<Customer_Home />}>
+          <Route index element={<Navigate to='dashboard' replace />} />
+          <Route path='dashboard' element={<CustomerDashboard />} />
+        </Route>
+        <Route path='/dropship' element={<Drop_Ship_Home />}>
+          <Route index element={<Navigate to='dashboard' replace />} />
+          <Route path='dashboard' element={<DropShipDashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ Frontend/src/Dropshipper/Pages/DropShipDashboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DropShipDashboard = () => {
+  return (
+    <div>DropShipDashboard</div>
+  )
+}
